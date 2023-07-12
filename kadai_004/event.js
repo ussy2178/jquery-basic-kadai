5f77c9ae50b5bfb69586b349e36a26f7cbe83f7d
Reed_Waker <fb21678@gmail.com>
2023-07-12 21:33:26 +0900

    Add files via upload

--- kadai_004/event.js
+++ kadai_004/event.js
@@ -1,12 +1,9 @@
 
-
+$(window).on('load', function() {
+    console.log('loadイベントが発生しました');
+});
 
 $(function(){
-
-    $(window).on('load', function() {
-        console.log('loadイベントが発生しました');
-    });
-    
     $(window).on('scroll', function(){
         console.log('scrollイベントが発生しました');
     });
